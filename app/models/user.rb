dc251d63952d945cad2d2088afbd14285d2ccf8e
kangkyu <kangkyu1111@gmail.com>
2014-11-12 09:44:19 -0800

    add validation of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-has_many :comments
-has_many :posts
+  has_many :comments
+  has_many :posts
+
+  validates :name, presence: true
+  validates :email, presence: true,                   
+                    format: /\A\S+@\S+\z/,
+                    uniqueness: { case_sensitive: false }
 end
